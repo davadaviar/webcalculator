4866ccd9860cb0285821b8e35ded1f3c0b421fb0
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-05-14 13:51:40 +0700

    Add function update & clear data calculator

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,1 +1,33 @@
-console.log("Anda menambahkan JavaScript pada web ini");
+// Menyimpan data dan kondisi kalkulator
+const calculator = {
+
+    displayNumber: "0",
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+
+}
+
+// Fungsi update angka di layar dan menghapus data pada kalkulator
+function updateDispaly() {
+
+    document.querySelector("#displayNumber").innerHTML = calculator.displayNumber;
+
+}
+
+function clearCalculator() {
+
+    calculator.displayNumber = displayNumber,
+    calculator.operator = null,
+    calculator.firstNumber = null,
+    calculator.waitingForSecondNumber = false
+
+}
+
+// Fungsi memasukkan digit angka ke displayNumber
+function inputDigit(digit) {
+
+    calculator.displayNumber += digit;  // Teori operator x += y  -> x = x + y
+
+}
+
